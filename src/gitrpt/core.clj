c9335e9e8019b9ce474b4c0aee5f10209a762354
Nalaka Jayasena <437578+nalaka@users.noreply.github.com>
2026-04-22 00:24:05 +0530

    feat: generate a pdf

--- src/gitrpt/core.clj
+++ src/gitrpt/core.clj
@@ -1,5 +1,19 @@
 (ns gitrpt.core
+  (:require [clj-pdf.core :refer [pdf]])
   (:gen-class))
 
+(defn generate-report [output-path]
+
+  (pdf
+    [{}
+     [:list {:roman true}
+      [:chunk {:style :bold} "a bold item"]
+      "another item"
+      "yet another item"]
+     [:phrase "some text"]
+     [:phrase "some more text"]
+     [:paragraph "yet more text"]]
+    output-path))
+
 (defn -main [& args]
-  (println "gitrpt"))
+  (generate-report "report.pdf"))
